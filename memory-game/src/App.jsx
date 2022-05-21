37427aa4b32e739da70c1a6dbc6826e9dc3023cd
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-05-21 21:35:23 +0530

    memory-game

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 import "./App.css";
-const cardImages = [
+const small = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+];
+
+const medium = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+];
+const large = [
   { src: "/img/helmet-1.png", matched: false },
   { src: "/img/potion-1.png", matched: false },
   { src: "/img/ring-1.png", matched: false },
@@ -10,14 +21,14 @@ const cardImages = [
   { src: "/img/shield-1.png", matched: false },
   { src: "/img/sword-1.png", matched: false },
 ];
-
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [game, setGame] = useState(large);
   const suffledCard = () => {
-    const suffledCard = [...cardImages, ...cardImages]
+    const suffledCard = [...game, ...game]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
     setCards(suffledCard);
@@ -56,11 +67,26 @@ function App() {
     setChoiceTwo(null);
     setTurns((prev) => prev + 1);
   };
+  const updateLevel = (event) => {
+    if (event.target.value === "small") {
+      setGame(small);
+    } else if (event.target.value === "medium") {
+      setGame(medium);
+    } else {
+      setGame(large);
+    }
+    suffledCard();
+  };
   console.log(cards);
   return (
     <div className="App">
       <h1>Memory Game</h1>
       <button onClick={suffledCard}>New Game</button>
+      <select onChange={updateLevel}>
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+      </select>
       <div className="card-grid">
         {cards.map((card) => (
           <Card
